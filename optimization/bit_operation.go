4fa0b33ab45bb7970d8aa67f162cdf6f5397fe8b
sp <121480653@qq.com>
2018-10-07 19:13:46 +0800

    代码提交

--- optimization/bit_operation.go
+++ optimization/bit_operation.go
@@ -29,6 +29,25 @@ func ByteReverse(a int) int {
     return a
 }
 
+// 十进制反向填充(int转*[]byte)
+// 来自官方标准库：/src/log/log.go
+func itoa(buf *[]byte, i int, wid int) {
+	// Assemble decimal in reverse order.
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || wid > 1 {
+		wid--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
+	}
+
+	// i < 10
+	b[bp] = byte('0' + i)
+	*buf = append(*buf, b[bp:]...)
+}
+
 // 求1的个数
 func CalcOneNum(a int) int {
     a = ((a & 0xAAAA) >> 1) + (a & 0x5555)
